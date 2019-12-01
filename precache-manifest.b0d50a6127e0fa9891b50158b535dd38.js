e4d1359033b30b0b630ec6ac65b6aa7c8cdbeb85
=varsha.jadhav <=>
2019-12-01 12:50:14 +0530

    Updates

---
+++ precache-manifest.b0d50a6127e0fa9891b50158b535dd38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25611c7fb2d95c74db860facda7d23d2",
+    "url": "/shaadi-test/index.html"
+  },
+  {
+    "revision": "265086ce3f19dfe898ea",
+    "url": "/shaadi-test/static/css/2.0b405c91.chunk.css"
+  },
+  {
+    "revision": "82883e47caef068f1f01",
+    "url": "/shaadi-test/static/css/main.25947b0e.chunk.css"
+  },
+  {
+    "revision": "265086ce3f19dfe898ea",
+    "url": "/shaadi-test/static/js/2.4d65da9d.chunk.js"
+  },
+  {
+    "revision": "82883e47caef068f1f01",
+    "url": "/shaadi-test/static/js/main.fd029940.chunk.js"
+  },
+  {
+    "revision": "c519a89ecddf69c80254",
+    "url": "/shaadi-test/static/js/runtime-main.7bed9a43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5eb79942415b94d5fe3a2bb054313c2c.js"
+  "/shaadi-test/precache-manifest.b0d50a6127e0fa9891b50158b535dd38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shaadi-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
