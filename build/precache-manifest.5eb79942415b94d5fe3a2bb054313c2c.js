3cd91aac97a5859122fc52d0bff6ba4ab811c06d
=varsha.jadhav <=>
2019-12-01 12:40:51 +0530

    changes added for deployment

---
+++ build/precache-manifest.5eb79942415b94d5fe3a2bb054313c2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a49f300120a2991d3e5f7d410a79391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "265086ce3f19dfe898ea",
+    "url": "/static/css/2.0b405c91.chunk.css"
+  },
+  {
+    "revision": "82883e47caef068f1f01",
+    "url": "/static/css/main.25947b0e.chunk.css"
+  },
+  {
+    "revision": "265086ce3f19dfe898ea",
+    "url": "/static/js/2.4d65da9d.chunk.js"
+  },
+  {
+    "revision": "82883e47caef068f1f01",
+    "url": "/static/js/main.fd029940.chunk.js"
+  },
+  {
+    "revision": "a2ffcdeee69114b9d9cb",
+    "url": "/static/js/runtime-main.c4d46d78.js"
+  }
+]);
